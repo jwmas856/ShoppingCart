1f04fc3af2836973c6dc9b8d704b6f1d4c4f35d0
jwmas856 <49160846+jwmas856@users.noreply.github.com>
2020-01-30 10:51:40 -0700

    1/30/20 - Intents working. Must remove ArrayList and implement bundle.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,22 +11,25 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList shoppingCart = new ArrayList();
     double totalPrice;
     String m_Stuff;
+    String condition="SelectedItemsFromShoppingCart";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button addCodMW = findViewById(R.id.buttonCodMW);
         Button btnCheckout = findViewById(R.id.buttonCheckout);
-        final ArrayList shoppingCart = new ArrayList();
-
+        // Use a bundle with intents.
+        // https://www.youtube.com/watch?v=3YCkW7J9NP0
+        // Dont pass the ArrayList to different activities, use the bundle.
+        //1/30/29
         addCodMW.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 totalPrice+= 59.99;
-                shoppingCart.add("Call of Duty: Modern Warfare(2019)");
+                shoppingCart.add("Call of Duty: Modern Warfare(2019)   | $59.99");
                 }
         });
 
--- app/src/main/java/com/example/myapplication/ShoppingActivity.java
+++ app/src/main/java/com/example/myapplication/ShoppingActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,6 +20,17 @@ public class ShoppingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_shopping);
         arrayList= (ArrayList) getIntent().getSerializableExtra("selectedList");
         TextView t1 = findViewById(R.id.textView3);
-        t1.setText(arrayList.get(0).toString());
+        for(int i = 0; i < arrayList.size(); i++){
+            t1.append(arrayList.get(i).toString() + "\n");
+        }
+        Button backButton = findViewById(R.id.button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ShoppingActivity.this, MainActivity.class);
+                intent.putExtra("selectedList", arrayList);
+                startActivity(intent);
+            }
+        });
     }
 }
